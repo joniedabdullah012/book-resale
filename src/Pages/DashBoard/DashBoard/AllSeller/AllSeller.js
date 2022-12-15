416b3c77f8f897dc0ebe9ff20d96202c65c24713
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-12-16 00:10:35 +0600

    all seller data added

--- src/Pages/DashBoard/DashBoard/AllSeller/AllSeller.js
+++ src/Pages/DashBoard/DashBoard/AllSeller/AllSeller.js
@@ -1,9 +1,50 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllSeller = () => {
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users')
+            const data = await res.json();
+            return data;
+
+
+
+        }
+
+    })
     return (
         <div>
             <h2>All seller</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Verify Seller</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, i) => <tr key={user._id}>
+                                <th>{i + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td><button className='btn btn-primary btn-xs'>Verify seller</button></td>
+                                <td><button className='btn btn-danger btn-xs'>Delete</button></td>
+                            </tr>
+
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
 
         </div>
     );
