a0c059407e1cabdfc5bfa80f78b8e0902f4ba6f1
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-12-16 00:30:46 +0600

    verifyed seller dashboard

--- src/Pages/DashBoard/DashBoard/AllSeller/AllSeller.js
+++ src/Pages/DashBoard/DashBoard/AllSeller/AllSeller.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-hot-toast';
 
 const AllSeller = () => {
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users')
@@ -13,7 +14,23 @@ const AllSeller = () => {
 
         }
 
-    })
+    });
+
+    const handleVerify = id => {
+        fetch(`http://localhost:5000/users/verify/${id}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+
+                if (data.modifiedCount > 0) {
+
+                    toast.success(' verify successfully')
+                    refetch();
+                }
+
+            })
+    }
     return (
         <div>
             <h2>All seller</h2>
@@ -35,7 +52,7 @@ const AllSeller = () => {
                                 <th>{i + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td><button className='btn btn-primary btn-xs'>Verify seller</button></td>
+                                <td>{user?.role !== 'verify' && <button onClick={() => handleVerify(user._id)} className='btn btn-primary btn-xs'>Verify seller</button>}</td>
                                 <td><button className='btn btn-danger btn-xs'>Delete</button></td>
                             </tr>
 
